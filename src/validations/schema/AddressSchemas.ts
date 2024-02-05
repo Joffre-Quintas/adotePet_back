7200825190c5b5d3b3d17173d597c131068b9766
Álvaro <alvinholuis222@gmail.com>
2024-02-05 20:38:51 -0300

    fix validations feature 2

--- src/validations/schema/AddressSchemas.ts
+++ src/validations/schema/AddressSchemas.ts
@@ -3,21 +3,21 @@ import * as yup from 'yup'
 const cepSchema = yup
     .string()
     .matches(/^\d{8}$/, 'CEP deve conter 8 números sem simbolos.')
-    .required('Campo obrigatório!')
+    .required('Campo CEP obrigatório!')
 
 const getAddressSchema = yup.object({
     cep: cepSchema,
-    street: yup.string().required('Campo obrigatório!'),
-    number: yup.string().required('Campo obrigatório!')
+    street: yup.string().required('Campo rua obrigatório!'),
+    number: yup.string().required('Campo número obrigatório!')
 })
 
 const createAddressSchema = yup.object({
     cep: cepSchema,
-    street: yup.string().required('Campo obrigatório!'),
-    city: yup.string().required('Campo obrigatório!'),
-    neighborhood: yup.string().required('Campo obrigatório!'),
-    state: yup.string().length(2, 'Campo "Estado" deve conter apenas 2 caracteres').required('Campo obrigatório!'),
-    number: yup.string().required('Campo obrigatório!'),
+    street: yup.string().required('Campo rua obrigatório!'),
+    city: yup.string().required('Campo cidade obrigatório!'),
+    neighborhood: yup.string().required('Campo bairro obrigatório!'),
+    state: yup.string().length(2, 'Campo estado deve conter apenas 2 caracteres').required('Campo estado obrigatório!'),
+    number: yup.string().required('Campo número obrigatório!'),
     complement: yup.string().notRequired()
 
 })
