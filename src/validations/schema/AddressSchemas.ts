85a48b636d698431fdd8684d45a0c396c329b97e
Álvaro <alvinholuis222@gmail.com>
2024-02-05 21:21:16 -0300

    rename function

--- src/validations/schema/AddressSchemas.ts
+++ src/validations/schema/AddressSchemas.ts
@@ -1,27 +1,27 @@
 import * as yup from 'yup'
 
-function dinamicMessage(nome:string){
+function errorMessageFieldRequired(nome:string){
     return `Campo ${nome} obrigatório`
 }
 
 const cepSchema = yup
     .string()
     .matches(/^\d{8}$/, 'CEP deve conter 8 números sem simbolos.')
-    .required(dinamicMessage('CEP'))
+    .required(errorMessageFieldRequired('CEP'))
 
 const getAddressSchema = yup.object({
     cep: cepSchema,
-    street: yup.string().required(dinamicMessage('rua')),
-    number: yup.string().required(dinamicMessage('número'))
+    street: yup.string().required(errorMessageFieldRequired('rua')),
+    number: yup.string().required(errorMessageFieldRequired('número'))
 })
 
 const createAddressSchema = yup.object({
     cep: cepSchema,
-    street: yup.string().required(dinamicMessage('rua')),
-    city: yup.string().required(dinamicMessage('cidade')),
-    neighborhood: yup.string().required(dinamicMessage('bairro')),
-    state: yup.string().length(2, 'Campo estado deve conter apenas 2 caracteres').required(dinamicMessage('estado')),
-    number: yup.string().required(dinamicMessage('número')),
+    street: yup.string().required(errorMessageFieldRequired('rua')),
+    city: yup.string().required(errorMessageFieldRequired('cidade')),
+    neighborhood: yup.string().required(errorMessageFieldRequired('bairro')),
+    state: yup.string().length(2, 'Campo estado deve conter apenas 2 caracteres').required(errorMessageFieldRequired('estado')),
+    number: yup.string().required(errorMessageFieldRequired('número')),
     complement: yup.string().notRequired()
 
 })
